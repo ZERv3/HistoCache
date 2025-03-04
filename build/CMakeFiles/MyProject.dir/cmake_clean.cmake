ee450a94c25885373bdcfc47ec61fc595c1eda28
Z≡Rv3.0_beta <128105093+ZERv3@users.noreply.github.com>
2025-03-04 20:59:01 +0300

    Configured CMakeLists

---
+++ build/CMakeFiles/MyProject.dir/cmake_clean.cmake
@@ -0,0 +1,19 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MyProject.dir/Histogram/Histogram.cpp.o"
+  "CMakeFiles/MyProject.dir/Histogram/Histogram.cpp.o.d"
+  "CMakeFiles/MyProject.dir/Tests/DictionaryTests.cpp.o"
+  "CMakeFiles/MyProject.dir/Tests/DictionaryTests.cpp.o.d"
+  "CMakeFiles/MyProject.dir/Tests/LoadTestsCache.cpp.o"
+  "CMakeFiles/MyProject.dir/Tests/LoadTestsCache.cpp.o.d"
+  "CMakeFiles/MyProject.dir/Tests/TestsCache.cpp.o"
+  "CMakeFiles/MyProject.dir/Tests/TestsCache.cpp.o.d"
+  "CMakeFiles/MyProject.dir/main.cpp.o"
+  "CMakeFiles/MyProject.dir/main.cpp.o.d"
+  "MyProject"
+  "MyProject.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/MyProject.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
